9b4ab21bb4e45f76b36df724831bb1cb1cb4582e
PauSubdelegado <paumorant19@gmail.com>
2025-12-09 12:29:39 +0100

    Sesion 2 ACT 1 COMPLETADA

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return "Listado de posts"; //SESION 1 COMPLETADA
 });
+
+Route::get('/posts/{id}', function ($id) {
+    return "Ficha del post $id"; 
+})->where("id", "[0-9]+"); // Sesion 2 ACT 1 COMPLETADA
