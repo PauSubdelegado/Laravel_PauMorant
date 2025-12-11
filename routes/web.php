733504733149d2278f38d59b9955523887717897
PauSubdelegado <paumorant19@gmail.com>
2025-12-11 10:37:15 +0100

    Sesion 2 ACT 1 COMPLETADA CORREGIDA

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("inicio");
 Route::get('/posts', function () {
     return "Listado de posts"; //SESION 1 COMPLETADA
-});
+})->name("posts_listado");
 
 Route::get('/posts/{id}', function ($id) {
-    return "Ficha del post $id"; 
-})->where("id", "[0-9]+"); // Sesion 2 ACT 1 COMPLETADA
+    return "Ficha del post $id";
+})->where("id", "[0-9]+")->name("posts_ficha"); // Sesion 2 ACT 1 COMPLETADA
